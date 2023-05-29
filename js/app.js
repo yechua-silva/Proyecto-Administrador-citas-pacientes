007035b925ecfe67896796dc7dcd9d9f31db43bf
yechua-silva <yechua_silva@outlook.cl>
2023-05-29 13:44:54 -0400

    Agrege la opcion de editar el objeto de la cita

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ const sintomasInput = document.querySelector('#sintomas');
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
+let editando;
+
 class Citas {
     constructor() {
         this.citas = [];
@@ -98,10 +100,19 @@ class UI {
             btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
             btnEliminar.innerHTML = `Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        `
+            </svg>
+            `
+            btnEliminar.onclick = () => eliminarCita(id);
 
-        btnEliminar.onclick = () => eliminarCita(id);
+            // Botón para editar cita
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+            </svg>
+            `;
+
+            btnEditar.onclick = () => cargarEdicion(cita);
 
             // Agregar los parrafos al divCita
             divCita.appendChild(mascotaParrafo);
@@ -113,6 +124,9 @@ class UI {
 
             // Agregar botón al div
             divCita.appendChild(btnEliminar);
+
+            // Agregar botón de editar
+            divCita.appendChild(btnEditar);
 
             // Agregar las citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -173,11 +187,33 @@ function nuevaCita(e) {
         return
     }
 
-    // General un id único
-    citasObj.id = Date.now();
+    // Comprobar modo edicion
+    if (editando) {
+        console.log('Editando');
+        ui.imprimirAlerta('Editado correctamente')
 
-    // Creando una nueva cita
-    administrarCitas.agregarCita({...citasObj}) // Crea un copia
+        // Pasar el objeto de al cita a edicion
+
+
+        // Cambiar texto del botón
+        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita'
+
+        // Deshabilitar el modo edicion
+        editando = false;
+
+    } else { // Cuando se crea la cita
+        console.log('Nueva cita');
+        // General un id único
+        citasObj.id = Date.now();
+
+        // Creando una nueva cita
+        administrarCitas.agregarCita({...citasObj}) // Crea un copia
+
+        // Mensaje de agregado correctamente
+        ui.imprimirAlerta('Se agrego correctamente');
+    }
+
+
 
     // Reiniciar objeto para la validación
     reiniciarObjeto();
@@ -207,4 +243,31 @@ function eliminarCita(id) {
 
     // Refrescar las citas
     ui.imprimirCitas(administrarCitas)
+}
+
+// Carga los datos y el modo edicion
+function cargarEdicion(cita) {
+    const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+
+    // Llenar los input
+    mascotaInput.value = mascota;
+    propietarioInput.value = propietario;
+    telefonoInput.value = telefono;
+    fechaInput.value = fecha;
+    horaInput.value = hora;
+    sintomasInput.value = sintomas;
+
+    // Llenar el objeto
+    citasObj.mascota = mascota;
+    citasObj.propietario = propietario;
+    citasObj.telefono = telefono;
+    citasObj.fecha = fecha;
+    citasObj.hora = hora;
+    citasObj.sintomas = sintomas;
+    citasObj.id = id;
+
+    // Modificar el texto del botón
+    formulario.querySelector('button[type=submit]').textContent = 'Guardar cambios';
+
+    editando = true;
 }
